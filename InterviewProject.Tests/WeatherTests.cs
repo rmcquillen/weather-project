b62c15d30b6b1725e70ef732603c6ba043e50be9
Ryan McQuillen <rdmcquillen@gmail.com>
2022-12-04 13:42:58 -0500

    Added test project
    Added test to catch situation where Celcius -> Fahrenheit calculation on Temperature model unexpectedly changes

---
+++ InterviewProject.Tests/WeatherTests.cs
@@ -0,0 +1,26 @@
+using InterviewProject.Models;
+
+namespace InterviewProject.Tests
+{
+    public class WeatherTests
+    {
+        [Fact]
+        public void TestFahrenheitCalculation()
+        {
+            // Arrange
+            var dailyForecast1 = new DailyForecast { Temperature = new Temperature { Maximum = new Maximum { ValueC = 0 } } };
+            var dailyForecast2 = new DailyForecast { Temperature = new Temperature { Maximum = new Maximum { ValueC = 100 } } };
+            var dailyForecast3 = new DailyForecast { Temperature = new Temperature { Maximum = new Maximum { ValueC = 16.7 } } };
+
+            // Assert
+            Assert.True(dailyForecast1.Temperature.Maximum.ValueF == ConvertCelciusToFahrenheit(dailyForecast1.Temperature.Maximum.ValueC));
+            Assert.True(dailyForecast2.Temperature.Maximum.ValueF == ConvertCelciusToFahrenheit(dailyForecast2.Temperature.Maximum.ValueC));
+            Assert.True(dailyForecast3.Temperature.Maximum.ValueF == ConvertCelciusToFahrenheit(dailyForecast3.Temperature.Maximum.ValueC));
+        }
+
+        private double ConvertCelciusToFahrenheit(double degreesC)
+        {
+            return Math.Round(32 + (degreesC / 0.5556));
+        }
+    }
+}
